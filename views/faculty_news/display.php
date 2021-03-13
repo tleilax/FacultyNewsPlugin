a92be704baa5a393605d4a0b4a6d97ea796870ef
Jan-Hendrik Willms <tleilax+github@gmail.com>
2021-03-13 14:41:01 +0100

    re #11

--- views/faculty_news/display.php
+++ views/faculty_news/display.php
@@ -37,44 +37,44 @@
             <span><?= $_('Keine Ankündigungen vorhanden') ?></span>
         <? else: ?>
             <? foreach ($new['news'] as $entry) : ?>
-                <? $is_new = $entry['chdate'] >= object_get_visit($entry['news_id'], 'news', false, false); ?>
+                <? $is_new = $entry['chdate'] >= object_get_visit($entry->news_id, 'news', false, false); ?>
                 <? $user = new User($entry['user_id']); ?>
                 <article class="studip toggle <?= ContentBoxHelper::classes($new['institut']->institut_id, $is_new) ?>" data-visiturl="<?= $controller->link_for('facultyNews/visit/' . $entry['news_id'], ['dummy' => '']) ?>">
                     <header>
                         <h1>
                             <a href="<?= URLHelper::getLink('dispatch.php/start', [
                                 'contentbox_open' => Request::get('contentbox_open'),
-                                'news_id_open' => $entry['news_id'],
+                                'news_id_open' => $entry->news_id,
                             ]) ?>">
                                 <?= Icon::create($is_new ? 'news+new' : 'news') ?>
-                                <?= htmlReady($entry['topic']) ?>
+                                <?= htmlReady($entry->topic) ?>
                             </a>
                         </h1>
                         <nav>
                             <a href="<?= URLHelper::getLink('dispatch.php/profile', ['username' => $user->username]) ?>">
                                 <?= htmlReady($entry['author']) ?>
                             </a>
-                            <span><?= strftime('%x', $entry['date']) ?></span>
-                            <span style="color: #050"><?= object_return_views($entry['news_id']) ?></span>
+                            <span><?= strftime('%x', $entry->date) ?></span>
+                            <span style="color: #050"><?= object_return_views($entry->news_id) ?></span>
                         <? if ($new['isAdmin']) : ?>
                             <?= ActionMenu::get()->addLink(
-                                URLHelper::getLink('dispatch.php/news/edit_news/' . $entry['news_id']),
+                                URLHelper::getLink('dispatch.php/news/edit_news/' . $entry->news_id),
                                 $_('Bearbeiten'),
                                 Icon::create('admin'),
                                 [
-                                    'title'       => sprintf($_('Bearbeiten von %s'), htmlReady($entry['topic'])),
+                                    'title'       => sprintf($_('Bearbeiten von %s'), htmlReady($entry->topic)),
                                     'data-dialog' => '',
                                 ]
                             )->addLink(
-                                URLHelper::getLink('', array('delete_news' => $entry['news_id'])),
+                                URLHelper::getLink('', array('delete_news' => $entry->news_id)),
                                 $_('Löschen'),
                                 Icon::create('trash'),
-                                ['title' => sprintf($_('Löschen von %s'), htmlReady($entry['topic']))]
+                                ['title' => sprintf($_('Löschen von %s'), htmlReady($entry->topic))]
                             )->render() ?>
                         <? endif; ?>
                         </nav>
                     </header>
-                    <section><?= formatReady($entry['body']) ?></section>
+                    <section><?= formatReady($entry->body) ?></section>
                 </article>
             <? endforeach; ?>
         <? endif; ?>
